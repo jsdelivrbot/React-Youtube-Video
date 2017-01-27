cf3c0f74d2d30346bbcc0a7f4513a3aab7882ae0
津崎 <i1658119@cse.kyoto-su.ac.jp>
2017-01-27 19:21:57 +0900

    READMEを変更した

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyAW4L6aC9v6ApCYRSPNiK9I_eCl-s3CYt0';
+//input Youtube APIkey
+const API_KEY = '';
 
 class App extends Component {
     constructor(props){
