78163afb4c8f4f823aa59125c46aae31cdeb28ae
津崎 <i1658119@cse.kyoto-su.ac.jp>
2017-01-27 18:40:14 +0900

    動画サムネイルの表示

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 //Create newComponent
 //some <html>
-import React from 'react';
+import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const App = () => {
-    return <div>Hi!</div>;
+const API_KEY = 'AIzaSyAW4L6aC9v6ApCYRSPNiK9I_eCl-s3CYt0';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos:[]};
+
+        YTSearch({ key:API_KEY,term:'pripara'} , (videos) => {
+            this.setState({ videos });
+            //this.setState({ videos:videos });
+        });
+
+    }
+    render(){
+    return (
+        <div>
+            <SearchBar />
+            <VideoDetail video={this.state.videos[0]} />
+            <VideoList  videos={this.state.videos} />
+        </div>
+        );
+    }
 }
 
 
